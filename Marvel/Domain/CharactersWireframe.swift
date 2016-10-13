f93d05b8a84da2f9a391e2917d3e0d21fcc6a59c
Ivan Ruiz Monjo <ivan.ruiz@scmspain.com>
2016-10-13 22:56:22 +0200

    added wireframe

---
+++ Marvel/Domain/CharactersWireframe.swift
@@ -0,0 +1,26 @@
+//
+//  CharactersWireframe.swift
+//  Marvel
+//
+//  Created by Ivan Ruiz Monjo on 13/10/16.
+//  Copyright © 2016 Ivan Ruiz Monjo. All rights reserved.
+//
+
+import Foundation
+
+class CharactersWireframe {
+
+    struct identifiers {
+        static let sb = "Main"
+        static let listCharacters = String(describing: ListCharactersViewController.self)
+    }
+
+    private var storyboard: UIStoryboard { return UIStoryboard(name: identifiers.sb, bundle: nil) }
+
+    func listCharacters() -> ListCharactersViewController {
+        guard let charactersVC = storyboard.instantiateViewController(withIdentifier: identifiers.listCharacters) as? ListCharactersViewController else { fatalError() }
+
+        return charactersVC
+
+    }
+}
