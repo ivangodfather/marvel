2e7cd0adfbaf0c87bdc5de4426418dbb0d898de5
Ivan Ruiz <ivan.ruiz@scmspain.com>
2016-10-15 09:49:51 +0200

    pretty gui

--- Marvel/DetailCharacterHeaderCollectionReusableView.swift
+++ Marvel/DetailCharacterHeaderCollectionReusableView.swift
@@ -12,9 +12,23 @@ class DetailCharacterHeaderCollectionReusableView: UICollectionReusableView {
 
     static let identifier = String(describing: DetailCharacterHeaderCollectionReusableView.self)
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var textWrapper: UIView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        textWrapper.clipsToBounds = true
+        textWrapper.layer.cornerRadius = 5
     }
-    
+
+    func configureWithCharacter(character: MarvelCharacter) {
+        characterImageView.image = #imageLiteral(resourceName: "avatar")
+        textLabel.text = character.description
+        if let thumb = character.thumbnail {
+            characterImageView.imageURL = thumb
+        }
+        accessibilityLabel = "\(character.id) \(character.name)"
+    }
 }
--- Marvel/DetailCharacterViewController.swift
+++ Marvel/DetailCharacterViewController.swift
@@ -13,6 +13,8 @@ import IOStickyHeader
 class DetailCharacterViewController: BaseViewController {
     var presenter: DetailCharacterPresenter!
 
+    var currentCharacter: MarvelCharacter!
+
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +23,9 @@ class DetailCharacterViewController: BaseViewController {
     }
 
     private func setupCollectionView() {
-        if let layout: IOStickyHeaderFlowLayout = self.collectionView.collectionViewLayout as? IOStickyHeaderFlowLayout {
-            layout.parallaxHeaderReferenceSize = CGSize(width: UIScreen.main.bounds.size.width, height: 274)
-            layout.parallaxHeaderMinimumReferenceSize = CGSize(width: UIScreen.main.bounds.size.width, height: 180)
+        if let layout = self.collectionView.collectionViewLayout as? IOStickyHeaderFlowLayout {
+            layout.parallaxHeaderReferenceSize = CGSize(width: UIScreen.main.bounds.size.width, height: 240)
+            layout.parallaxHeaderMinimumReferenceSize = CGSize(width: UIScreen.main.bounds.size.width, height: 60)
             layout.itemSize = CGSize(width: UIScreen.main.bounds.size.width, height: layout.itemSize.height)
             layout.parallaxHeaderAlwaysOnTop = true
             layout.disableStickyHeaders = true
@@ -43,6 +45,7 @@ class DetailCharacterViewController: BaseViewController {
 
 extension DetailCharacterViewController: DetailCharacterUI {
     func show(character: MarvelCharacter) {
+        self.currentCharacter = character
         self.title = character.name
     }
 }
@@ -50,7 +53,7 @@ extension DetailCharacterViewController: DetailCharacterUI {
 extension DetailCharacterViewController: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return 30
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -59,12 +62,8 @@ extension DetailCharacterViewController: UICollectionViewDataSource {
     }
 
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailCharacterHeaderCollectionReusableView.identifier, for: indexPath) as! DetailCharacterHeaderCollectionReusableView
-
+        guard let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailCharacterHeaderCollectionReusableView.identifier, for: indexPath) as? DetailCharacterHeaderCollectionReusableView else { fatalError() }
+        cell.configureWithCharacter(character: currentCharacter)
         return cell
-    }
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
     }
 }
