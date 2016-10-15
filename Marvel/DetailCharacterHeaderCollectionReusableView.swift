95f0e206afc3e6ffc09a68c7b1bb9dc8f4faf2d9
Ivan Ruiz <ivan.ruiz@scmspain.com>
2016-10-15 09:08:10 +0200

    added library and loaded

---
+++ Marvel/DetailCharacterHeaderCollectionReusableView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailCharacterHeaderCollectionReusableView.swift
+//  Marvel
+//
+//  Created by Ivan Ruiz on 15/10/16.
+//  Copyright © 2016 Ivan Ruiz Monjo. All rights reserved.
+//
+
+import UIKit
+
+class DetailCharacterHeaderCollectionReusableView: UICollectionReusableView {
+
+    static let identifier = String(describing: DetailCharacterHeaderCollectionReusableView.self)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
--- Marvel/DetailCharacterViewController.swift
+++ Marvel/DetailCharacterViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import Foundation
+import IOStickyHeader
+
 
 class DetailCharacterViewController: BaseViewController {
     var presenter: DetailCharacterPresenter!
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
         presenter.viewDidLoad()
+    }
+
+    private func setupCollectionView() {
+        if let layout: IOStickyHeaderFlowLayout = self.collectionView.collectionViewLayout as? IOStickyHeaderFlowLayout {
+            layout.parallaxHeaderReferenceSize = CGSize(width: UIScreen.main.bounds.size.width, height: 274)
+            layout.parallaxHeaderMinimumReferenceSize = CGSize(width: UIScreen.main.bounds.size.width, height: 180)
+            layout.itemSize = CGSize(width: UIScreen.main.bounds.size.width, height: layout.itemSize.height)
+            layout.parallaxHeaderAlwaysOnTop = true
+            layout.disableStickyHeaders = true
+            self.collectionView.collectionViewLayout = layout
+        }
+
+        self.collectionView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 0, 0)
+        let headerNib = UINib(nibName: String(describing: DetailCharacterHeaderCollectionReusableView.self), bundle: nil)
+        collectionView.register(headerNib, forSupplementaryViewOfKind: IOStickyHeaderParallaxHeader, withReuseIdentifier: DetailCharacterHeaderCollectionReusableView.identifier)
+
+        collectionView.dataSource = self
     }
 
 }
@@ -21,5 +42,26 @@ class DetailCharacterViewController: BaseViewController {
 extension DetailCharacterViewController: DetailCharacterUI {
     func show(character: MarvelCharacter) {
         self.title = character.name
+    }
+}
+
+extension DetailCharacterViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: DetailCharacterHeaderCollectionReusableView.identifier, for: indexPath) as! DetailCharacterHeaderCollectionReusableView
+
+        return cell
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
     }
 }
