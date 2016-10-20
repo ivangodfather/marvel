a6908d7cc3bb05595192ae25db7a5e1b2a2d42ef
Ivan Ruiz Monjo <ivanruizmonjo@gmail.com>
2016-10-20 23:03:40 +0200

    clean code

--- Marvel/CharactersAPI.swift
+++ Marvel/CharactersAPI.swift
@@ -11,28 +11,30 @@ import RxAlamofire
 import RxSwift
 
 class MarvelAPIEndpoints {
-
+    
     let parser: MarvelAPIParser
-
+    
     init(parser: MarvelAPIParser = MarvelAPIParser()) {
         self.parser = parser
     }
-
+    
     func getCharacters(params: MarvelAPI.CharactersParams) -> Observable<[MarvelCharacter]> {
-        return RxAlamofire.requestJSON(.get,
-                                       MarvelAPI.url,
-                                       parameters: params.toDict() as [String : AnyObject]?)
+        return RxAlamofire
+            .requestJSON(.get,
+                         MarvelAPI.url,
+                         parameters: params.toDict() as [String : AnyObject]?)
             .flatMap { response, data -> Observable<[MarvelCharacter]> in
                 guard response.statusCode == 200 else { return Observable.error(MarvelError.apiError) }
                 guard let dict = data as? [String: Any] else { return Observable.error(MarvelError.unkown) }
                 return Observable.just(self.parser.parse(dict: dict))
         }
     }
-
+    
     func getComics(params: MarvelAPI.ComicParams) -> Observable<[MarvelComic]> {
-        return RxAlamofire.requestJSON(.get,
-                                       MarvelAPI.urlComics(characterId: params.characterId),
-                                       parameters: params.toDict() as [String : AnyObject]?)
+        return RxAlamofire
+            .requestJSON(.get,
+                         MarvelAPI.urlComics(characterId: params.characterId),
+                         parameters: params.toDict() as [String : AnyObject]?)
             .flatMap { response, data -> Observable<[MarvelComic]> in
                 guard response.statusCode == 200 else { return Observable.error(MarvelError.apiError) }
                 guard let dict = data as? [String: Any] else { return Observable.error(MarvelError.unkown) }
